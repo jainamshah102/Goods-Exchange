caa2b682d627a0b2730a1616ad20a302b4a9c121
swanand2001 <swanand.apte@somaiya.edu>
2021-10-01 16:34:42 +0530

    merged register and login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -35,3 +35,35 @@ module.exports.userRegister = async (req, res, next) => {
         next(new InvalidParameters("Invalid Parameters", 400));
     }
 };
+
+
+module.exports.userLogin = async (req,res,next) =>{
+    try{
+        const { useranme, password } =await UserRegisterJoi(req.body);
+        if (!(useranme && password)) {
+            res.status(400).send("All fields are required");
+        }
+
+        const user = await User.findOne({ useranme });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+              { user_id: user._id, email },
+              process.env.TOKEN_KEY,
+              {
+                expiresIn: "2h",
+              }
+            );
+      
+            // save user token
+            user.token = token;
+      
+            // user
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+    } catch(err){
+        next(err);
+    }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const UserRouter = express.Router();
 const userController = require("../controllers/user.controller");
 const authController = require("../controllers/auth.controller");
-const userLogin = require("../controllers/userlogin.controller");
+
 
 UserRouter.post("/register", userController.userRegister);
-UserRouter.post("/login",userLogin);
+UserRouter.post("/login",userController.userLogin);
 
 
 module.exports = UserRouter;
