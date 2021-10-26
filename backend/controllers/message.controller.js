3ad085f6d82e7f65ee0349c97a3e5e55d238aa3b
swanand2001 <swanand.apte@somaiya.edu>
2021-10-26 14:39:20 +0530

    Message route get

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,51 @@
+const InvalidParameters = require("../errors/invalidParameters");
+const NotFound = require("../errors/notFoundError");
+const Message = require("../models/message.model");
+const ProductJoi = require("../joiValidation/product.joi");
+const Message = require('../models/message.model');
+module.exports.convquery = async (req, res, next) => {
+    let user1 = mongoose.Types.ObjectId(jwtUser.id);
+    let user2 = mongoose.Types.ObjectId(req.query.userId);
+    Message.aggregate([
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'to',
+                foreignField: '_id',
+                as: 'toObj',
+            },
+        },
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'from',
+                foreignField: '_id',
+                as: 'fromObj',
+            },
+        },
+    ])
+        .match({
+            $or: [
+                { $and: [{ to: user1 }, { from: user2 }] },
+                { $and: [{ to: user2 }, { from: user1 }] },
+            ],
+        })
+        .project({
+            'toObj.password': 0,
+            'toObj.__v': 0,
+            'toObj.date': 0,
+            'fromObj.password': 0,
+            'fromObj.__v': 0,
+            'fromObj.date': 0,
+        })
+        .exec((err, messages) => {
+            if (err) {
+                console.log(err);
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({ message: 'Failure' }));
+                res.sendStatus(500);
+            } else {
+                res.send(messages);
+            }
+        });
+};
