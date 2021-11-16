c08e411655de5c173c86ce4ce6e2b82625da8d61
swanand2001 <swanand.apte@somaiya.edu>
2021-11-16 19:03:56 +0530

    description limit

--- frontend/src/components/ViewProduct.js
+++ frontend/src/components/ViewProduct.js
@@ -30,7 +30,7 @@ const ViewProduct =  () => {
               <div className="card shadow-sm">
                 <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns={prod.primaryImage} role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">{prod.primaryImage}</text></svg>
                 <div className="card-body">
-                  <p className="card-text">{prod.title}-{prod.description}</p>
+                  <p className="card-text">{prod.title}-{prod.description.substring(0,50)}...</p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
