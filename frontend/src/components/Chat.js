9e32c45d8c41117fd2f27a9ee94bbdb6a74b9103
swanand2001 <swanand.apte@somaiya.edu>
2021-11-25 23:09:21 +0530

    send btn update

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,12 +1,13 @@
 import { StylesContext } from '@material-ui/styles'
 import React from 'react'
 import '../css/styles.css'
+import sendbtn from '../sendbtn.png'
 const Chat = () => {
     return (
         <div className="container my-4">
             <div className="row">
                 <div className="col-sm" style={{border:"2px solid black",borderRadius:"13px", maxWidth:"500px"}}>
-                    Chats
+                    <p style={{textAlign:"center",color:"#008080",opacity:"0.8",fontWeight:"900",fontFamily:"sans-serif"}}>CHATS</p>
             
                     <ul className="list-group my-4">
                         <li className="list-group-item" style={{height:"54px"}}>Jainam</li>
@@ -21,9 +22,9 @@ const Chat = () => {
                     <div className="container2">
                     </div> 
                     <div className="send">
-                        <form action="#" id="send-container" style={{float:'right'}}>
-                            <input type="text" name="messageInp" id="messageInp"/>
-                            <button className="btn" type="submit">Send</button>
+                        <form action="#" className="d-flex" id="send-container" style={{float:'right'}}>
+                            <input type="text" name="messageInp" id="messageInp" style={{width:"720px"}} /> 
+                            <button className="btn" type="submit"><img src={sendbtn} style={{width:"20px",height:"20px"}}/></button>
                         </form>
                     </div>
                 </div>
