ba2184e8bb1d486142a9d1a2cc33d8750d384f4b
dhairyasalot <dhairya.salot@somaiya.edu>
2021-12-14 18:48:40 +0530

    changes to footer and search bar

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Container>
+          <Row>
+            <Col className='text-center py-3'>
+            
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </footer>
+  )
+}
