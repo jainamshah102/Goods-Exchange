9a26177a98eacf70bd714104bdb22643d8f1e63a
dhairyasalot <dhairya.salot@somaiya.edu>
2021-12-16 23:40:43 +0530

    transaction list ui

---
+++ frontend/src/containers/NotFound.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h4>Not found page @_@ .</h4>
+}
---
+++ frontend/src/containers/index.js
@@ -0,0 +1,29 @@
+import App from './App'
+import Home from './Home'
+import NotFound from './NotFound'
+import ProductCreate from './Product/ProductCreate'
+import ProductDetails from './Product/ProductDetails'
+import ProductEdit from './Product/ProductEdit'
+import ProductList from './Product/ProductList'
+import RequestTo from './RequestTo'
+import TransactionDetails from './TransactionDetails'
+import TransactionList from './TransactionList'
+import UserLogin from './User/UserLogin'
+import UserProfile from './User/UserProfile'
+import UserRegister from './User/UserRegister'
+
+export {
+  App,
+  Home,
+  ProductDetails,
+  ProductCreate,
+  ProductEdit,
+  ProductList,
+  UserRegister,
+  UserProfile,
+  RequestTo,
+  TransactionDetails,
+  TransactionList,
+  NotFound,
+  UserLogin,
+}
